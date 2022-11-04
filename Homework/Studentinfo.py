f616e5853bbd2943d015709e23b783f7b2e2b75e
Назар <patrisisgenius@gmail.com>
2022-11-04 23:36:49 +0200

    Добавление вопроса "Как у тебя дела?"

---
+++ Homework/Studentinfo.py
@@ -0,0 +1,4 @@
+from settings import settings
+
+student = settings(name='')
+print(student)
---
+++ Homework/settings.py
@@ -0,0 +1,30 @@
+import datetime
+
+class settings:
+    name = ''
+    year = ''
+    group = ''
+    rating = 4.5
+
+    def __init__(self, name='', year='', group='',rating=4.5):
+        self.name = input('Введите ваше имя:')
+        self.year = input('В каком году вы родились?')
+        self.group = input('Введите вашу группу:')
+        self.rating = rating
+
+    def get_age(self):
+
+
+
+
+    def __str__(self):
+        return f' == {self.name} ==\n' \
+               f'Возраст: {self.year}\n' \
+               f'Группа: {self.group}\n' \
+               f'Средний бал: {self.rating}\n' \
+
+
+
+
+
+
